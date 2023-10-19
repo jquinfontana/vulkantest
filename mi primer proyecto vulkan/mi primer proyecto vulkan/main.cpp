9a1548641250f4ef11c7580e10a581faf6a7dece
jquinfontana <jquin2000@hotmail.com>
2023-10-19 20:57:03 -0300

    primer commit

---
+++ mi primer proyecto vulkan/mi primer proyecto vulkan/main.cpp
@@ -0,0 +1,66 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+//#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    //variables
+    GLFWwindow* window;
+    VkInstance instance;
+
+    //metodos
+    void initWindow() {
+        //inicializo glfw
+        glfwInit();
+        //le pido que no me abra un cnexto opengl, le pido que no se pueda reescalar
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        //creo la ventana con sus propiedades
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        //glfw
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
