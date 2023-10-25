cc163189a28ef6e721b821e4d72448f766de7b34
jquinfontana <jquin2000@hotmail.com>
2023-10-25 20:07:52 -0300

    agrego validation layers y empiezo divice pick

--- mi primer proyecto vulkan/mi primer proyecto vulkan/main.cpp
+++ mi primer proyecto vulkan/mi primer proyecto vulkan/main.cpp
@@ -1,11 +1,25 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-//#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.h> esto es para incluir vulkan si no lo incluimos con glfw
 
+//librerias
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
+//configuracion validation layers
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+//parametros de configuracion de la app
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -22,6 +36,7 @@ private:
     //variables
     GLFWwindow* window;
     VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
     //metodos
     void initWindow() {
@@ -34,8 +49,123 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
+    void createInstance() {
+        //informacion de la aplicacion, puede resultar util para que el driver realice optimizaciones especificas para nuestra app (es opcional)
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        //no opcional, especifica las extenciones y capas de validacion
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        //pregunto y seteo las extensiones requeridas por glwf
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        //agrego capas de validacion
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        //VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        //mejor verificar resuktado como en el siguiente codigo (sin guardar el resultado)
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+
+        //muestro informacion opcional con fines didacticos
+        //imprimo las extensiones disponibles
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        std::cout << "available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        //imprimo las que necesita glfw
+        std::cout << "\nextenciones requeridas por glwf:\n" ;
+        for (size_t i = 0; i < glfwExtensionCount; i++)
+        {
+            std::cout << '\t' << glfwExtensions[i] << '\n';
+        }
+
+        //llamo a la funcion para hacer lo mismo de las extensiones pero con las vl
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+    }
+
+    bool checkValidationLayerSupport() {
+        //imprimo y checkeo por las validation layers analog a las extensiones
+        uint32_t layerCount = 0;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+        std::cout << "\navailable layers:\n";
+        for (const auto& layer : availableLayers) {
+            std::cout << '\t' << layer.layerName << '\n';
+        }
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+            if (!layerFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+        std::cout << "\navailable GPUs:\n";
+        for (const auto& device : devices) {
+            std::cout << '\t' << device << '\n';
+        }
+        for (const auto& device : devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device) {
+        return true;
+    }
+
     void initVulkan() {
         createInstance();
+        //stupdebugmessage() agregar la funcion, tutorial en la seccion validation layers
+        pickPhysicalDevice();
     }
 
     void mainLoop() {
@@ -45,6 +175,8 @@ private:
     }
 
     void cleanup() {
+        //vulkan
+        vkDestroyInstance(instance, nullptr);
         //glfw
         glfwDestroyWindow(window);
         glfwTerminate();
