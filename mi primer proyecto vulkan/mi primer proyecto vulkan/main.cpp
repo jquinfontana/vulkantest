86579e0f83fc2bd1caa2e30df967ee319160ef11
jquinfontana <jquin2000@hotmail.com>
2023-12-18 23:00:03 -0300

    cosas de BLAS

--- mi primer proyecto vulkan/mi primer proyecto vulkan/main.cpp
+++ mi primer proyecto vulkan/mi primer proyecto vulkan/main.cpp
@@ -149,6 +149,9 @@ private:
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
 
+    //RAY TRACING
+    PFN_vkCmdTraceRaysKHR pfn_vkCmdTraceRaysKHR;
+
     //bandera rezize
     bool framebufferResized = false;
 
@@ -1019,6 +1022,7 @@ private:
         createSurface();
         pickPhysicalDevice();
         createLogicalDevice();
+        getAndWrapFunctionFromGPU();
         createSwapChain();
         createImageViews();
         createRenderPass();
@@ -1032,29 +1036,88 @@ private:
 
     //RAY TRACING
     void getAndWrapFunctionFromGPU() {
-        PFN_vkCmdTraceRaysKHR pfn_vkCmdTraceRaysKHR = (PFN_vkCmdTraceRaysKHR)vkGetDeviceProcAddr(device, "vkCmdTraceRaysKHR");
+        pfn_vkCmdTraceRaysKHR = (PFN_vkCmdTraceRaysKHR)vkGetDeviceProcAddr(device, "vkCmdTraceRaysKHR");
+    }
+    VKAPI_ATTR void VKAPI_CALL vkCmdTraceRaysKHR(
+        VkCommandBuffer commandBuffer,
+        const VkStridedDeviceAddressRegionKHR* pRaygenShaderBindingTable,
+        const VkStridedDeviceAddressRegionKHR* pMissShaderBindingTable,
+        const VkStridedDeviceAddressRegionKHR* pHitShaderBindingTable,
+        const VkStridedDeviceAddressRegionKHR* pCallableShaderBindingTable,
+        uint32_t width,
+        uint32_t height,
+        uint32_t depth)
+    {
+        assert(pfn_vkCmdTraceRaysKHR);
+        pfn_vkCmdTraceRaysKHR(
+            commandBuffer,
+            pRaygenShaderBindingTable,
+            pMissShaderBindingTable,
+            pHitShaderBindingTable,
+            pCallableShaderBindingTable,
+            width,
+            height,
+            depth);
+    }
+    inline VkDeviceAddress getBufferDeviceAddress(VkDevice device, VkBuffer buffer)
+    {
+        if (buffer == VK_NULL_HANDLE)
+            return 0ULL;
 
-        VKAPI_ATTR void VKAPI_CALL vkCmdTraceRaysKHR(
-            VkCommandBuffer commandBuffer,
-            const VkStridedDeviceAddressRegionKHR * pRaygenShaderBindingTable,
-            const VkStridedDeviceAddressRegionKHR * pMissShaderBindingTable,
-            const VkStridedDeviceAddressRegionKHR * pHitShaderBindingTable,
-            const VkStridedDeviceAddressRegionKHR * pCallableShaderBindingTable,
-            uint32_t width,
-            uint32_t height,
-            uint32_t depth)
-        {
-            assert(pfn_vkCmdTraceRaysKHR);
-            pfn_vkCmdTraceRaysKHR(
-                commandBuffer,
-                pRaygenShaderBindingTable,
-                pMissShaderBindingTable,
-                pHitShaderBindingTable,
-                pCallableShaderBindingTable,
-                width,
-                height,
-                depth);
-        };
+        VkBufferDeviceAddressInfo info = { VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO };
+        info.buffer = buffer;
+        return vkGetBufferDeviceAddress(device, &info);
+    }
+    void objectToVkGeometryKHR(VkBuffer buffer) {
+        // BLAS builder requires raw device addresses.
+        VkDeviceAddress vertexAddress = getBufferDeviceAddress(device, buffer);
+        //VkDeviceAddress indexAddress = nvvk::getBufferDeviceAddress(m_device, m_indexBuffer.buffer);
+
+        uint32_t maxPrimitiveCount = 1;
+
+        // Describe buffer as array of VertexObj.
+        VkAccelerationStructureGeometryTrianglesDataKHR triangles{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_TRIANGLES_DATA_KHR };
+        triangles.vertexFormat = VK_FORMAT_R32G32B32_SFLOAT;  // vec3 vertex position data.
+        triangles.vertexData.deviceAddress = vertexAddress;
+        triangles.vertexStride = sizeof(glm::vec3);
+        // Describe index data (32-bit unsigned int)
+        //triangles.indexType = VK_INDEX_TYPE_UINT32;
+        //triangles.indexData.deviceAddress = indexAddress;
+        // Indicate identity transform by setting transformData to null device pointer.
+        triangles.transformData = {0};
+        triangles.maxVertex = 3 - 1;
+
+        // Identify the above data as containing opaque triangles.
+        VkAccelerationStructureGeometryKHR asGeom{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_KHR };
+        asGeom.geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
+        asGeom.flags = VK_GEOMETRY_OPAQUE_BIT_KHR; //for anyhit
+        asGeom.geometry.triangles = triangles;
+
+        // The entire array will be used to build the BLAS.
+        VkAccelerationStructureBuildRangeInfoKHR offset;
+        offset.firstVertex = 0;
+        offset.primitiveCount = maxPrimitiveCount;
+        offset.primitiveOffset = 0;
+        offset.transformOffset = 0;
+
+        VkAccelerationStructureBuildGeometryInfoKHR buildInfo{};
+        buildInfo.sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_GEOMETRY_INFO_KHR;
+        buildInfo.type = VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR;
+        buildInfo.flags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR;
+        buildInfo.mode = VK_BUILD_ACCELERATION_STRUCTURE_MODE_BUILD_KHR;
+        buildInfo.geometryCount = 1;
+        buildInfo.pGeometries = &asGeom;
+        buildInfo.srcAccelerationStructure = VK_NULL_HANDLE;
+
+        VkAccelerationStructureBuildSizesInfoKHR sizeInfo{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_SIZES_INFO_KHR };
+        vkGetAccelerationStructureBuildSizesKHR(device, VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR, &buildInfo,
+            &offset.primitiveCount, &sizeInfo);
+
+        /*VkAccelerationStructureCreateInfoKHR acceleration_structure_create_info{};
+        acceleration_structure_create_info.sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_INFO_KHR;
+        acceleration_structure_create_info.buffer = 
+        acceleration_structure_create_info.type = VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR;*/
+
     }
 
     void mainLoop() {
